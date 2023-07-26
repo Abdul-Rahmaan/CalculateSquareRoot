faf17588b02e05b0dea1945945198dc67cf57ac6
Abdur Rehman <abdurrehmanmughal1@gmail.com>
2023-07-26 12:47:00 +0500

    feat:CalculateSquareRoot

---
+++ CalculateSquareRoot/CalculateSquareRoot/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CalculateSquareRoot
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            int num, sq = 1;
+            Console.Write("enter any number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            test(num);
+
+            Console.Write("the squareroot of number is : ");
+            Console.WriteLine(test(num));
+
+        }
+
+        public static int test(double number)
+            {
+                int sq = 1;
+                //int num;
+                while (sq < number / sq)
+                {
+                    sq++;
+                }
+                if (sq > number / sq) return sq - 1;
+                return sq;
+
+                //Console.ReadLine();
+            }
+
+        
+    }
+}
